3120c495045d64d4a029dedb7710b326564af4bf
Aida <aidaacha@ucm.es>
2019-04-04 18:54:55 +0200

    Enemigo Eks

---
+++ Assets/Scripts/Eks.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eks : MonoBehaviour
+{
+
+    public float bulletSpeed;
+    public float spawnTime;
+    public Transform BulletSpawner;
+    public GameObject bulletPrefab;
+
+    private float counter;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > spawnTime)
+        {
+
+            counter = 0;
+        }
+    }
+
+}
