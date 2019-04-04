9ed713357244632b3bccfe134de4783ac70aa405
Dewei <deweiche@ucm.es>
2019-04-04 13:49:41 +0200

    MovJugador para que no deslice

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     const float maxSpeed = 5;//ponemos un maximo de velocidad se puede poner publico
     public float acceleration, jumpForce;
     private float moveX;
+    bool saltando;
+    public GameObject Suelo;
 
     bool jump;
     int tiempoinmune;
-    Vector2 movement,dirGancho;
+    Vector2 movement, dirGancho;
     Rigidbody2D rb;
-    
+
     //daño
     private bool mov = true;
     public float Timedmg;
@@ -24,8 +26,6 @@ public class PlayerController : MonoBehaviour
     public float velocidadEnganchado;
     bool enganchado = false;
     Vector3 posGancho, posGanchoInicio;
-    bool colision;
-    bool cambio = false;
 
     private void Start()
     {
@@ -38,24 +38,25 @@ public class PlayerController : MonoBehaviour
         }
         dirGancho = Vector2.right;
         posGanchoInicio = gancho.transform.localPosition;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
     private void Update()
     {
+
+        //Nos aseguramos de que sea dinámico  
         if (!enganchado && mov)
         {
-//Nos aseguramos de que sea dinámico  
+
             //Leo entrada de teclado para moverme en el ejeX
             moveX = Input.GetAxis("Horizontal");
+            // Para que el jugador se pare al soltal el teclado
+             if (!saltando && Input.GetButtonUp("Derecha") || (!saltando && Input.GetButtonUp("Izquierda")))
+             {
+                 rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+             }
+            if (!mov) { moveX = 0; }
 
-            /*//para parar al jugador cuando deje de moverse y no se deslice
-            if (Input.GetButtonUp("Derecha")|| Input.GetButtonUp("Izquierda"))
-            {
-                cambio = true;
-            }*/
-            
-            if (!mov) { moveX = 0;  }
-
-            //Salto cuando pulso el boton de salto y si estoy en el suelo
+            //Salto  si estoy en el suelo
             if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f) //Nos aseguramos de que el jugador esté en el suelo comparando su rb.velocity.y
             {
                 jump = true;
@@ -65,8 +66,6 @@ public class PlayerController : MonoBehaviour
             //(para saber donde disparar el gancho)
             if (Input.GetButton("Derecha"))
             {
-                
-
                 //dirGancho = Vector2.right;
                 dirGancho = Vector2.right;
                 //esta condicion es necesaria porque sino mientras el gancho esta en ida/vuelta y el jugador rota, el gancho tambien
@@ -75,12 +74,12 @@ public class PlayerController : MonoBehaviour
                     //cambia la rotacion en el eje Y del player cuando se mueve a la izquierda
                     transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, 0);
                 }
-
-
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             }
+
             else if (Input.GetButton("Izquierda"))
             {
-
                 dirGancho = Vector2.right;
                 //esta condicion es necesaria porque sino mientras el gancho esta en ida/vuelta y el jugador rota, el gancho tambien
                 if (gancho.daEstado() == HookState.Quieto)
@@ -88,8 +87,8 @@ public class PlayerController : MonoBehaviour
                     //pone la rotacion en el eje Y del jugador a 0 si se mueve a la derecha
                     transform.rotation = new Quaternion(transform.rotation.x, -180, transform.rotation.z, 0);
                 }
-
-
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             }
             else if (Input.GetKey(KeyCode.S))
             {
@@ -98,43 +97,30 @@ public class PlayerController : MonoBehaviour
             }
             else if (Input.GetKey(KeyCode.W))
             {
-
                 dirGancho = Vector2.up;
-
             }
-
-
-            GameManager.instance.direccion(dirGancho);//pasa la direccion al GameManager
         }
-        else if (enganchado )  //Estado Enganchado
+        else if (enganchado)  //Estado Enganchado
         {
-
-           // GetComponent<Rigidbody2D>().isKinematic = true;//lo cambiamos a kinematic para moverlo con transform
-
             float step = velocidadEnganchado * Time.deltaTime;
             posGancho = gancho.GetComponent<Transform>().position;
             if (transform.position.x < posGancho.x) posGancho -= posGanchoInicio;
             else posGancho += posGanchoInicio;
             //var heading = posGancho - transform.position;
             //Debug.Log("heading"+heading);
-           // if(transform.position.x<posGancho.x) transform.Translate(heading.x * Time.deltaTime, heading.y * Time.deltaTime, 0);
+            // if(transform.position.x<posGancho.x) transform.Translate(heading.x * Time.deltaTime, heading.y * Time.deltaTime, 0);
             //else transform.Translate(-heading.x * Time.deltaTime, heading.y * Time.deltaTime, 0);
             transform.position = Vector2.MoveTowards(transform.position, posGancho, step);
             //transform.position = Vector2.Lerp(transform.position, posGancho, step);
-            //print(" tiempo " + Time.deltaTime);
-            //print(" step " + step);
-            //print("posGancho: " + posGancho);
-            //print("");
             if (Vector2.Distance(transform.position, gancho.transform.position) < 0.8f)
             {
-
                 gancho.cambiaEstado(HookState.Quieto);
-                
                 CambiaEstado(false);
                 rb.isKinematic = false;
             }
-            
+
         }
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.down) * 0.6f, Color.red);
     }
     private void FixedUpdate()//Mueve al personaje
     {
@@ -142,35 +128,32 @@ public class PlayerController : MonoBehaviour
         {
             //Vector de movimiento
             movement = new Vector2(moveX, 0);
-            if (rb != null && Mathf.Abs(rb.velocity.x) < maxSpeed ) //Limitamos la velocidad del jugador con el rb.velocity.x
+            if (rb != null && Mathf.Abs(rb.velocity.x) < maxSpeed) //Limitamos la velocidad del jugador con el rb.velocity.x
             {
-               /*//para que no se deslice
-                if (cambio)
-                {
-                    rb.velocity = new Vector2(0, rb.velocity.y);
-                }
-                //para que se pueda volver a mover
-                if (movement.x == 0)  cambio = false;
-                */
-                rb.AddForce(movement * acceleration); //Movimiento horizontal
-                
-                //Debug.Log("la velocity"+rb.velocity);
+                /* //para que no se deslice
+                  if (cambio)
+                  {
+                      rb.velocity = new Vector2(0, rb.velocity.y);
+                  }
+                  //para que se pueda volver a mover
+                  if (movement.x == 0)  cambio = false;*/
+
+                rb.AddForce(movement * acceleration); //Movimiento 
             }
-            if(rb.velocity.y < -10)//para que no caiga muy rapido
+            if (rb.velocity.y < -5)//para que no caiga muy rapido
             {
                 rb.velocity = new Vector2(rb.velocity.x, -10);
             }
-            /*else if (rb != null && moveX==0) //Limitamos la velocidad del jugador con el rb.velocity.x
-            {
-                Debug.Log("HOLA");
-                rb.velocity = new Vector2(0,0);
-            }*/
+            //Salto
+            //La variable jump se hace falsa despues de hacer el salto para que no se ejecute el salto mas veces en el FixedUpdate
             if (jump)
             {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);   //Salto
-                jump = false;                                               //La variable jump se hace falsa despues de hacer el salto para que no se ejecute el salto mas veces en el FixedUpdate
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                jump = false;
+                saltando = true;
             }
         }
+
     }
 
     /*****************************************************************************
@@ -183,30 +166,26 @@ public class PlayerController : MonoBehaviour
  *****************************************************************************/
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
         if (enganchado == true)
         {
             GetComponent<Rigidbody2D>().isKinematic = false;//Movimiento dinamico si player colisiona 
-            //Debug.Log("colisiona");
             gancho.cambiaEstado(HookState.Quieto);
             CambiaEstado(false);
         }
     }
-
-
+     private void OnCollisionEnter2D(Collision2D Suelo)
+     {
+         saltando = false;
+     }
 
     //realiza un pequeño salto al entrar en contacto con un enemigo y sufrir daño
     public void EnemyKnockBack(float enemyPosX)
     {
         jump = true;
-
         float side = Mathf.Sign(enemyPosX - transform.position.x); //devuelve -1, 0 o 1 dependiendo de si un numero es positivo, negativo o 0. Asi sabremos la direccion donde aplicar la fuerza. 
         rb.AddForce(Vector2.left * side * jumpForce / 2, ForceMode2D.Impulse); //aplicamos una fuerza diagonal
         mov = false;//desactivamos mov mientras estemos sufriendo daño
-       
         Invoke("EnableMovementandColision", Timedmg);//volvemos activar mov despues de un tiempo
-        
-
     }
     //activa/desactiva el movimiento
     public bool EnabledMovement(bool valor)
@@ -221,13 +200,21 @@ public class PlayerController : MonoBehaviour
         if (estado)
         {
             GetComponent<Rigidbody2D>().isKinematic = true;
-            //GetComponent<Rigidbody2D>().simulated = false;
+            GetComponent<Rigidbody2D>().simulated = false;
+            if (Input.GetButtonDown("Jump"))
+            {
+                Debug.Log("SOÑEMOS");
+                jump = true;
+                gancho.cambiaEstado(HookState.Vuelta);
+                CambiaEstado(false);
+            }
         }
-        
+
         else
         {
+
             GetComponent<Rigidbody2D>().isKinematic = false;
-            //GetComponent<Rigidbody2D>().simulated = true;
+            GetComponent<Rigidbody2D>().simulated = true;
 
         }
     }
