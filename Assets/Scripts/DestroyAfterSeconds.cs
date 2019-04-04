43ea54b06f2ec812b927f6ae75d97fa1d7f7b325
Aida <aidaacha@ucm.es>
2019-04-04 18:45:47 +0200

    Añadir enemigo Eks

---
+++ Assets/Scripts/DestroyAfterSeconds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour {
+    public float seconds;
+
+	//destruye el objeto pasado seconds segundos
+	void Start () {
+        Destroy(gameObject, seconds);
+	}
+}
