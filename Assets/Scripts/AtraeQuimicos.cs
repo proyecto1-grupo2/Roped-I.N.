2d447633da8504d506baf578b2848a268271c0df
Andrés de la Cuesta <ancues02@ucm.es>
2019-04-06 18:47:40 +0200

    Algunos cambios

--- Assets/Scripts/AtraeQuimicos.cs
+++ Assets/Scripts/AtraeQuimicos.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AtraeQuimicos : MonoBehaviour {
-    
+public class AtraeQuimicos : MonoBehaviour
+{
+
     public LayerMask Mask;
     Rigidbody2D Cajarb;
     //Animator ani;
@@ -12,27 +13,30 @@ public class AtraeQuimicos : MonoBehaviour {
     bool colision;
     public Transform target;
 
-   
+
     void OnTriggerEnter2D(Collider2D other)// metodo para detectar colision
     {
         // condicion para detectar si colisiona con el gancho del player y 
-        if (other.gameObject.CompareTag("Gancho") )
+        if (other.gameObject.CompareTag("Gancho"))
         {
+            //Debug.Log("enganchado");
             hook = other.gameObject.GetComponent<MovGancho>();
             colision = true;
+            //EnganchaQuimico();
+            Enganche();
         }
 
         else if (hook != null)//para que cuando el gancho suelte al quimico, el gancho vuelva
         {
             hook.cambiaEstado(HookState.Vuelta);
         }
-       
+
         else colision = false;
-        
+
     }
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (hook!=null && other.gameObject.CompareTag("Suelo"))
+        if (hook != null && other.gameObject.CompareTag("Suelo"))
         {
             hook.cambiaEstado(HookState.Vuelta);
         }
@@ -56,33 +60,68 @@ public class AtraeQuimicos : MonoBehaviour {
     }
 
 
-    void Update()
+    //void Update()
+    //{
+
+    //    /*
+    //     *Si un quimico colisiona con el gancho se hace hijo del gancho
+    //     * para ello es necesario que el gancho no tenga hijos(no tenga mas quimicos)
+    //     */ 
+    //    if (colision)
+    //    {
+    //        if (hook.transform.childCount == 0)
+    //        {
+    //            transform.GetComponent<BoxCollider2D>().isTrigger = true;
+    //            transform.SetParent(target.transform);
+    //            transform.GetComponent<Rigidbody2D>().isKinematic = true;
+    //        }
+    //        else colision = false;
+
+    //        //hook.cambiaEstado(HookState.Vuelta);
+
+    //    }
+
+
+    //    /*else
+    //    {
+    //        transform.GetComponent<Rigidbody2D>().isKinematic = false;
+    //    }*/
+
+    //}
+
+    public void Enganche()
     {
-
-        /*
-         *Si un quimico colisiona con el gancho se hace hijo del gancho
-         * para ello es necesario que el gancho no tenga hijos(no tenga mas quimicos)
-         */ 
-        if (colision)
+        if (hook.transform.childCount == 0)
         {
-            if (hook.transform.childCount == 0)
-            {
-                transform.GetComponent<BoxCollider2D>().isTrigger = true;
-                transform.SetParent(target.transform);
-                transform.GetComponent<Rigidbody2D>().isKinematic = true;
-            }
-            else colision = false;
-            
-            //hook.cambiaEstado(HookState.Vuelta);
-
+            Debug.Log("enganchado");
+            ModifyQuimico(true, hook.transform);
         }
-       
-
-        /*else
+        else
         {
-            transform.GetComponent<Rigidbody2D>().isKinematic = false;
-        }*/
-
+            ModifyQuimico(false, null);
+        }
     }
-    
+
+    void ModifyQuimico(bool enganchado, Transform parent)
+    {
+        transform.GetComponent<BoxCollider2D>().isTrigger = enganchado;
+        transform.SetParent(parent);
+        transform.GetComponent<Rigidbody2D>().isKinematic = enganchado;
+    }
+    //void EnganchaQuimico()
+    //{
+    //    if (hook.transform.childCount == 0)
+    //    {
+    //        transform.GetComponent<BoxCollider2D>().isTrigger = true;
+    //        transform.SetParent(target.transform);
+    //        transform.GetComponent<Rigidbody2D>().isKinematic = true;
+    //    }
+    //}
+
+    //public void DesenganchaQuimico()
+    //{
+    //    transform.GetComponent<BoxCollider2D>().isTrigger = false;
+    //    transform.parent = null;
+    //    transform.GetComponent<Rigidbody2D>().isKinematic = false;
+    //}
 }
