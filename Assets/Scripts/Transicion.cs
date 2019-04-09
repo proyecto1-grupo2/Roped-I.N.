a8b43713132cc347705745d63ad821bc7e1960bb
Pablo Rodriguez <prodra02@ucm.es>
2019-04-09 18:29:22 +0200

    Añadida zona de transición

---
+++ Assets/Scripts/Transicion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transicion : MonoBehaviour {
+    public string nextLevel;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+        {
+            GameManager.instance.ChangeScene(nextLevel);
+        }
+    }
+}
