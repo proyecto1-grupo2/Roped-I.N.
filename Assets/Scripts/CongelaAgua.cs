51f118f05225ea9830c89bfd2a1388bf3a8b4106
Pablo Rodriguez <prodra02@ucm.es>
2019-04-24 20:00:30 +0200

    Añadida animación de AguaCongelada

--- Assets/Scripts/CongelaAgua.cs
+++ Assets/Scripts/CongelaAgua.cs
@@ -7,6 +7,7 @@ public class CongelaAgua : MonoBehaviour
     //public float dragHielo, tCong;
     DeadZone dz;
     BoxCollider2D bc2D;
+    Animator anim;
 
     //Al colisionar un quimico de hielo con una zona de agua
     //    desactiva la DeadZone, la vuelve solida y aplica un
@@ -15,6 +16,7 @@ public class CongelaAgua : MonoBehaviour
     {
         dz = gameObject.GetComponent<DeadZone>();
         bc2D = gameObject.GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -24,9 +26,11 @@ public class CongelaAgua : MonoBehaviour
             bc2D.isTrigger = false;
             //gameObject.GetComponent<Rigidbody2D>().drag = dragHielo;
             Destroy(other.gameObject);
+            anim.SetBool("Congelada", true);
+            Debug.Log("Congelada");
             //Cambiar sprite (De momento solo cambia el tono a un más azulado)
-            SpriteRenderer sprit = gameObject.GetComponent<SpriteRenderer>();
-            sprit.material.color = Color.blue;
+            //SpriteRenderer sprit = gameObject.GetComponent<SpriteRenderer>();
+            //sprit.material.color = Color.blue;
             
             //Debug.Log("Drag " + gameObject.GetComponent<Rigidbody2D>().drag);
             //Invoke("FreezeOff", tCong);
