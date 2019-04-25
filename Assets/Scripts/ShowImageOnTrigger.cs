b0b880285b045b4d115506dc46f46b87073c87c4
Pablo Rodriguez Bobada <prodra02@ucm.es>
2019-04-25 21:41:57 +0200

    Añadido prefab PopUpEvent

---
+++ Assets/Scripts/ShowImageOnTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowImageOnTrigger : MonoBehaviour {
+    public SpriteRenderer image;
+    public float time;
+    private void Start()
+    {
+        HideImage();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+            image.gameObject.SetActive(true);
+        //Invoke("HideImage", time);
+    }
+
+    public void HideImage()
+    {
+        image.gameObject.SetActive(false);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+            image.gameObject.SetActive(false);
+    }
+
+}
