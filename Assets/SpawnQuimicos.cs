5ed46310fdbc7cbdc9d2fce3c3a0190489de4b88
Sebastian Sanchez-Hombria <sebsan01@ucm.es>
2019-05-10 00:46:52 +0200

    Respawner de quimicos

---
+++ Assets/SpawnQuimicos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnQuimicos : MonoBehaviour {
+
+    public GameObject Quimico;
+    public GameObject ObjetivoDelQuimico;
+    GameObject Quim;
+
+
+    // Use this for initialization
+    void Start () {
+        Quim = Instantiate(Quimico, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(transform.position, Quim.transform.position) > 5 && ObjetivoDelQuimico.activeSelf == true)
+        {
+            Quim = Instantiate(Quimico, transform.position, transform.rotation);
+        }
+
+    }
+}
