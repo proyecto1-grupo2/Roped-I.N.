e89046a9e439d32080504bf78231e385efac9334
Aida <aidaacha@ucm.es>
2019-05-16 16:57:47 +0200

    Animaciones daño turm

---
+++ Assets/Scripts/MuerteVoladores.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuerteVoladores : MonoBehaviour {
+
+    Death death;
+    bool dead;
+    Transform t;
+    Vector2 caida;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+        death = GetComponent<Death>();
+        dead = death.GetDead();
+        t = GetComponent<Transform>();
+        caida = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if (dead == true)
+        {
+            caida = new Vector2(0, speed * Time.deltaTime);
+        }
+	}
+}
